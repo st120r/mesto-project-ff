16148bb682e1e046b6f7fbf7e0166f69cc9ab7bf
st120r <49389823+st120r@users.noreply.github.com>
2024-07-04 11:12:05 +0300

    after review 3 diko tuplu nado bolshe spat

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,6 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const cardsContainer = document.querySelector('.places__list');
-const addButton = document.querySelector('.profile__add-button'); // кнопка добавления карточки
-const popupNewCard = document.querySelector('.popup_type_new-card'); // попап для новой карточки
-const popupCloseButtons = document.querySelectorAll('.popup__close'); // кнопки закрытия попапов
-const formNewCard = popupNewCard.querySelector('.popup__form'); // форма создания новой карточки
 
 // @todo: Функция создания карточки
 function createCard({ name, link }, deleteCallback) {
@@ -27,26 +23,6 @@ const deleteCard = (event) => {
   const item = event.target.closest('.card');
   item.remove();
 };
-
-// @todo: Обработчики событий для кнопок закрытия попапов
-popupCloseButtons.forEach(button => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopup(popup));
-});
-
-// @todo: Обработчик события для кнопки добавления карточки
-addButton.addEventListener('click', () => openPopup(popupNewCard));
-
-// @todo: Обработчик события для формы создания новой карточки
-formNewCard.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const cardTitle = formNewCard.querySelector('.popup__input_type_card-name').value;
-  const cardImage = formNewCard.querySelector('.popup__input_type_url').value;
-  const cardData = createCard({ name: cardTitle, link: cardImage }, deleteCard);
-  cardsContainer.prepend(cardData); // Добавляем новую карточку в начало списка
-  closePopup(popupNewCard); // Закрываем попап после добавления карточки
-  formNewCard.reset(); // Сбрасываем форму
-});
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(cardData => {
